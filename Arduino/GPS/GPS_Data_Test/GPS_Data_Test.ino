e3c4b806bfc0402ea14ad1744d746c3f326e542b
YeahNahIDK <reiddavidson0808@gmail.com>
2025-11-27 20:04:47 +1300

    Added Basic Setup and Data Parsing
    Fully functional.

--- Arduino/GPS/GPS_Data_Test/GPS_Data_Test.ino
+++ Arduino/GPS/GPS_Data_Test/GPS_Data_Test.ino
@@ -1,11 +1,42 @@
-#include <Adafruit_GPS>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h> // For Arduino UNO
+
+const uint8_t txPin = 3;
+const uint8_t rxPin = 2;
+SoftwareSerial gpsSerial(rxPin, txPin); // For Arduino UNO
+Adafruit_GPS GPS(&gpsSerial);
+
+String NMEA_RMC;
+String NMEA_GGA;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  GPS.begin(9600); // Default baud rate is 9600
+  GPS.sendCommand("$PGCMD, 33, 0,*6d"); // Disable antenna warning
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // Only output RMC and GGA
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ); 
+
+  delay(1000); // Let the commands get set
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  GPS.read();
 
+  if (GPS.newNMEAreceived()) {
+    GPS.parse(GPS.lastNMEA());
+
+    char* nmea = GPS.lastNMEA();
+
+    if (strncmp(nmea, "$GNRMC", 6) == 0) {
+        NMEA_RMC = String(nmea);
+        Serial.println(NMEA_RMC);
+    } else if (strncmp(nmea, "$GNGGA", 6) == 0) {
+        NMEA_GGA = String(nmea);
+        Serial.println(NMEA_GGA);
+    }
+
+    Serial.println(NMEA_RMC);
+    Serial.println(NMEA_GGA);
+  }
 }
