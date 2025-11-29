222dd1f1e317ef02a7e913dd592838dc3f241349
YeahNahIDK <reiddavidson0808@gmail.com>
2025-11-29 19:13:34 +1300

    Updated to work for ESP32

--- Arduino/GPS/GPS_Data_Test/GPS_Data_Test.ino
+++ Arduino/GPS/GPS_Data_Test/GPS_Data_Test.ino
@@ -1,18 +1,17 @@
 #include <Adafruit_GPS.h>
-#include <SoftwareSerial.h> // For Arduino UNO
 
-const uint8_t txPin = 3;
-const uint8_t rxPin = 2;
-SoftwareSerial gpsSerial(rxPin, txPin); // For Arduino UNO
-Adafruit_GPS GPS(&gpsSerial);
+const uint8_t rxPin = 12;
+const uint8_t txPin = 13;
+
+Adafruit_GPS GPS(&Serial1);
 
 void setup() {
   Serial.begin(115200);
 
+  Serial1.begin(9600, SERIAL_8N1, rxPin, txPin);
   GPS.begin(9600); // Default baud rate is 9600
-  GPS.sendCommand("$PGCMD, 33, 0,*6d"); // Disable antenna warning
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); // Only output RMC and GGA
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ); 
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);
 
   delay(1000); // Let the commands get set
 }
@@ -23,4 +22,5 @@ void loop() {
   if (GPS.newNMEAreceived()) {
     GPS.parse(GPS.lastNMEA());
     Serial.println(GPS.lastNMEA());
+  }
 }
