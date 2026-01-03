47d834f7cb8ee29124c354d3cbb7f7863c184cd5
YeahNahIDK <reiddavidson0808@gmail.com>
2026-01-03 19:39:17 +1300

    Changed buzzer.c to output to pin 4.0

--- Flight-Firmware/MCU/src/buzzer.c
+++ Flight-Firmware/MCU/src/buzzer.c
@@ -1,16 +1,16 @@
 // Active buzzer used - HIGH results in sound
 
 void buzzer_init() {
-    P2DIR = 0x01; // Pin 2.0 output, rest input
-    P2OUT = 0x00; // Set output to low for all states
+    P4DIR = 0x01; // Pin 2.0 output, rest input
+    P4OUT = 0x00; // Set output to low for all states
 }
 
 void buzzer_on() {
     // Digital write HIGH
-    P2OUT = 0x01;
+    P4OUT = 0x01;
 }
 
 void buzzer_off() {
     // Digital write LOW
-    P2OUT = 0x00;
+    P4OUT = 0x00;
 }
