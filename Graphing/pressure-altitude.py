ae7d20d828a326a93777f0481252fbf3a1d51a00
YeahNahIDK <reiddavidson0808@gmail.com>
2026-01-09 19:35:46 +1300

    Created, 3D graph: Pressure, Time, Displacement Z

---
+++ Graphing/pressure-altitude.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+# Getting annoying error, this directly finds the directory
+script_location = Path(__file__).resolve().parent
+file_path = script_location / "test-data.csv"
+
+df = pd.read_csv(file_path)
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+
+ax.plot(df["Time"], df["Pressure"], df["Disp_Z"], linewidth=2, alpha=1, color="Blue")
+
+# Make legend, set axes limits and labels
+ax.legend()
+ax.set_xlabel('Time')
+ax.set_ylabel('Pressure')
+ax.set_zlabel('Displacement Z')
+
+plt.show()
