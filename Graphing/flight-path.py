62547145892af0e9d99420b75625668eac444ded
YeahNahIDK <reiddavidson0808@gmail.com>
2026-01-09 19:27:43 +1300

    Cleaned up the graph to make labels more readable

--- Graphing/flight-path.py
+++ Graphing/flight-path.py
@@ -18,6 +18,7 @@ df['Acc_Mag'] = np.sqrt(df['Acc_X']**2 + df['Acc_Y']**2 + df['Acc_Z']**2)
 sc = ax.scatter(df['Disp_X'], df['Disp_Y'], df['Disp_Z'], c=df['Acc_Mag'], cmap='plasma', alpha=1)
 
 cbar = fig.colorbar(sc, ax=ax, pad=0.1)
+cbar.set_label('Acceleration Magnitude (m/s²)', rotation=270, labelpad=20)
 
 # Makes each axis unit the same size
 x_range = df["Disp_X"].max() - df["Disp_X"].min()
@@ -27,9 +28,13 @@ ax.set_box_aspect((x_range, y_range, z_range))
 
 # Make legend, set axes limits and labels
 ax.legend()
-ax.set_xlabel('Displacement X')
-ax.set_ylabel('Displacement Y')
-ax.set_zlabel('Displacement Z')
+ax.set_xlabel('Displacement X', labelpad=20)
+ax.set_ylabel('Displacement Y', labelpad=10)
+ax.set_zlabel('Displacement Z', labelpad=5)
+
+# Tick label rotations
+ax.tick_params(axis='x', rotation=50)
+ax.tick_params(axis='y', rotation=-50)
 
 # Customize the view angle so it's easier to see that the scatter points lie
 # on the plane y=0
