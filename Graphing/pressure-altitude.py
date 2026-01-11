0af407c7f31519476d2bf6f0cf9b767b608f28c5
YeahNahIDK <reiddavidson0808@gmail.com>
2026-01-12 12:21:40 +1300

    Added temperature overlay

--- Graphing/pressure-altitude.py
+++ Graphing/pressure-altitude.py
@@ -12,6 +12,10 @@ ax = fig.add_subplot(projection='3d')
 
 
 ax.plot(df["Time"], df["Pressure"], df["Disp_Z"], label="Pressure", linewidth=2, alpha=1, color="Blue")
+sc = ax.scatter(df["Time"], df["Pressure"], df['Disp_Z'], c=df['Temp'], cmap='plasma', alpha=1)
+
+cbar = fig.colorbar(sc, ax=ax, pad=0.1)
+cbar.set_label('Temperature', rotation=270, labelpad=20)
 
 # Make legend, set axes limits and labels
 ax.legend()
