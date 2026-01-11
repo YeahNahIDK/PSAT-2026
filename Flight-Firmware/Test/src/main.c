656c62568cdb8f282b3acd07f1228a3a70484162
YeahNahIDK <reiddavidson0808@gmail.com>
2026-01-12 12:38:41 +1300

    Added imports and function calls for setup()
    Delay before turning the buzzer off is still needed

--- Flight-Firmware/Test/src/main.c
+++ Flight-Firmware/Test/src/main.c
@@ -2,6 +2,16 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "lora.h"
+#include "gps.h"
+#include "barometer.h"
+#include "imu.h"
+#include "sdcard.h"
+#include "servo.h"
+#include "buzzer.h"
+#include "spi_manager.h"
+#include "uart.h"
+
 // ============================================================================
 // FUNCTION DECLARATIONS
 // ============================================================================
@@ -14,7 +24,7 @@ void loop(void);
 // ============================================================================
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD; // Stops watchdog timer
+    WDTCTL = WDTPW | WDTHOLD; // Stop WDT
     
     setup();
 
@@ -24,10 +34,36 @@ int main(void) {
 }
 
 void setup(void) {
-    // TODO: Initialize hardware
-    // TODO: Initialize communication interfaces
-    // TODO: Initialize sensors
-    // TODO: Run pre-flight tests
+    // Initialize communication interfaces
+    spi_init();
+    uart_init();
+
+    // Initialize hardware
+    lora_init();
+    gps_init();
+    baro_init();
+    imu_init();
+    sdcard_init();
+    servo_init();
+    buzzer_init();
+
+    // Run pre-flight tests
+    lora_send("Testing started\n");
+
+    // Check sensor data
+    gps_read();
+    imu_read();
+    baro_update();
+
+    lora_send(buffer);
+
+    // Check buzzer output
+    buzzer_on();
+    // Wait 5s
+    buzzer_off();
+    
+    // Confirm tests are complete
+    lora_send("Testing ended\n");
 }
 
 void loop(void) {
